1c4641546a5a7ab25ce3be7c75e35175dc4e026b
Sai Chandra <115443921+saichandra2332@users.noreply.github.com>
2026-02-05 19:44:11 +0530

    Update database.py

--- hrms-backend/database.py
+++ hrms-backend/database.py
@@ -2,10 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:srinu@localhost:5432/hrms_db"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
