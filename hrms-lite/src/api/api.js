092190a703200725a71dd66b26821b19ea5f70b3
Sai Chandra <115443921+saichandra2332@users.noreply.github.com>
2026-02-05 20:06:54 +0530

    Update api.js

--- hrms-lite/src/api/api.js
+++ hrms-lite/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://kind-rejoicing-production-4dc9.up.railway.app"
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
